feee276510bda15e84b29846aa157414d07f8bb7
Thomas De Groof <thomas@distinctgrey.com>
2014-08-07 15:12:25 +0200

    Initial commit. Chapter 1-7

---
+++ app/scripts/app.js
@@ -0,0 +1,43 @@
+/* global app:true */
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angNewsApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'firebase'
+]).constant('FIREBASE_URL', 'https://amber-fire-133.firebaseIO.com/');
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'AuthCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
