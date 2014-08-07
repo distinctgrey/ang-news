5bc6151c9ad345c43dd934a77982f40961262500
Thomas De Groof <thomas@distinctgrey.com>
2014-08-07 16:48:18 +0200

    Chapter 8 - user profile

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,7 +1,9 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope, $location, $routeParams, Post) {
-  $scope.posts = Post.all;
+  if ($location.path() === '/') {
+    $scope.posts = Post.all;
+  }
 
   $scope.deletePost = function (postId) {
     Post.delete(postId);
